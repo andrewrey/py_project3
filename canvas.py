dc51014e67d1d74512b02abbc2df8231f14809aa
Andrew Reynolds <andrew_reynolds@mac.com>
2021-03-25 00:57:16 -0700

    added input prompts for user to provide lenght width and colour of canvas

--- canvas.py
+++ canvas.py
@@ -1,15 +1,18 @@
 import numpy as np
 from PIL import Image
+from colour import Color
 
 
 
 
 class Canvas:
     def __init__(self, height, width, colour):
+        colours_rgb = list(Color(colour).rgb)
+        for i in range(len(colours_rgb)):
+            colours_rgb[i] = round(colours_rgb[i] * 255)
         self.width = width
         self.height = height
-        self.colour = colour
-
+        self.colour = colours_rgb
         # Generate the 3d array using numpy
         self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
         # update the depth for color pixel array
--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from canvas import Canvas
 from square import Square
 from rectangle import Rectangle
 
+canvas_width = int(input("Enter the width for you canvas: "))
+canvas_height = int(input("Enter the height of your canvas: "))
+canvas_colour = input("Enter the colour you want for Canvas: ")
 
 
-my_canvas = Canvas(1000, 1000, [22, 88, 220])
+my_canvas = Canvas(height=canvas_height, width=canvas_width, colour=canvas_colour)
 first_square = Square(0, 0, 500, [30, 55, 90])
 first_square.draw_square(my_canvas)
 first_rectangle = Rectangle(x=0, y=0, length=500, width=50, color=[200, 150, 66])
 first_rectangle.draw_rectangle(my_canvas)
 my_canvas.create_canvas('first_canvas.png')
+print(my_canvas.colour)
